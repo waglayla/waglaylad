0a5817681c96b5a354a252939a4708ba4b9e8350
TheKrueg1998 <80941977+TheKrueg1998@users.noreply.github.com>
2024-09-23 23:21:29 -0700

    Update start_daemon.go

--- cmd/waglaylawallet/start_daemon.go
+++ cmd/waglaylawallet/start_daemon.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/waglayla/waglaylad/cmd/pyrinwallet/daemon/server"
+import "github.com/waglayla/waglaylad/cmd/waglaylawallet/daemon/server"
 
 func startDaemon(conf *startDaemonConfig) error {
 	return server.Start(conf.NetParams(), conf.Listen, conf.RPCServer, conf.KeysFile, conf.Profile, conf.Timeout)
